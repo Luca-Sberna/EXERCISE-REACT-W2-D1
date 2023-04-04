af4e183508e0240a1c56b280cf9442534c25bfed
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-04-04 02:57:01 +0200

    adjust-delete

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { Alert } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
@@ -35,6 +36,35 @@ class AddComments extends Component {
     }
   };
 
+  fetchDeleteComment = async () => {
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/comments/${this.props.asin}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDEzYzU3YWM1NmIzNjAwMTMzZmU1N2MiLCJpYXQiOjE2ODA1NTIxMzIsImV4cCI6MTY4MTc2MTczMn0.ZHGVupEfAXnmeoOa0t1gPDXwjJwbavEljg6Q2zDq1MA",
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      if (response.ok) {
+        const updatedComments = this.state.comments.slice(0, 1);
+        this.setState({ comments: updatedComments });
+
+        <Alert variant="danger">è stato cancellato con successo!</Alert>;
+        console.log("Comment deleted successfully");
+      }
+    } catch (error) {
+      <Alert variant="danger">
+        Non ci sono commenti su questo libro, sii il primo ad inserirne uno per
+        cancellarlo!
+      </Alert>;
+      console.log("Error deleting comment: ", error);
+    }
+  };
+
   handleChange = (propertyName, propertyValue) => {
     this.setState({
       newComment: {
@@ -48,6 +78,11 @@ class AddComments extends Component {
   handleSubmit = async (e) => {
     e.preventDefault();
     this.fetchAddComments();
+  };
+
+  handleDelete = async (e) => {
+    e.preventDefault();
+    this.fetchDeleteComment();
   };
 
   render() {
@@ -77,6 +112,14 @@ class AddComments extends Component {
 
         <Button className="w-100 mt-2" variant="success" type="submit">
           Submit
+        </Button>
+        <Button
+          className="w-100 mt-2"
+          variant="danger"
+          type="button"
+          onClick={this.fetchDeleteComment}
+        >
+          Delete
         </Button>
       </Form>
     );
